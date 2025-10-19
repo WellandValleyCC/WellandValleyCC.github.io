cb95407b1eb608105fca390cae6925f5c7e1aa64
Mike Ives <mbives+wvcc@gmail.com>
2025-10-19 20:21:51 +0100

    Adds primary key to Competitor class

--- processor/ClubProcessor/Models/Competitor.cs
+++ processor/ClubProcessor/Models/Competitor.cs
@@ -1,15 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ClubProcessor.Models
 {
 	public class Competitor
 	{
+		[Key]
 		public required string ClubNumber { get; set; }       // e.g. "9999"
 		public required string Surname { get; set; }          // e.g. "Doe"
 		public required string GivenName { get; set; }        // e.g. "John"
 		public required string ClaimStatus { get; set; }      // e.g. "First Claim"
-		public required bool IsFemale { get; set; }                    // e.g. false
-		public required bool IsJuvenile { get; set; }                  // e.g. false
-		public required bool IsJunior { get; set; }                    // e.g. true
-		public required bool IsSenior { get; set; }                    // e.g. false
-		public required bool IsVeteran { get; set; }                   // e.g. false
+		public required bool IsFemale { get; set; }           // e.g. false
+		public required bool IsJuvenile { get; set; }         // e.g. false
+		public required bool IsJunior { get; set; }           // e.g. true
+		public required bool IsSenior { get; set; }           // e.g. false
+		public required bool IsVeteran { get; set; }          // e.g. false
 	}
 }
