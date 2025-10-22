630baff460400f409b63a43672a8811360efea20
Mike Ives <mbives+wvcc@gmail.com>
2025-10-22 22:18:58 +0100

    Migrations created
    PS C:\repos\wvcc\WellandValleyCC.github.io\processor\ClubProcessor> Remove-Item -Recurse -Force .\Migrations\
    PS C:\repos\wvcc\WellandValleyCC.github.io\processor\ClubProcessor> dotnet ef migrations add InitialCreate --context EventDbContext --output-dir Migrations/EventDb
    Build started...
    Build succeeded.
    Done. To undo this action, use 'ef migrations remove'
    PS C:\repos\wvcc\WellandValleyCC.github.io\processor\ClubProcessor> dotnet ef migrations add InitialCreate --context CompetitorDbContext --output-dir Migrations/CompetitorDb
    Build started...
    Build succeeded.
    Done. To undo this action, use 'ef migrations remove'

---
+++ processor/ClubProcessor/Migrations/CompetitorDb/20251022211004_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubProcessor.Migrations.CompetitorDb
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Competitors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClubNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", nullable: false),
+                    GivenName = table.Column<string>(type: "TEXT", nullable: false),
+                    ClaimStatus = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsFemale = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsJuvenile = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsJunior = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsSenior = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsVeteran = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastUpdatedUtc = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Competitors", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Competitors");
+        }
+    }
+}
---
+++ processor/ClubProcessor/Migrations/EventDb/20251022210926_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubProcessor.Migrations.EventDb
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rides",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EventNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClubNumber = table.Column<int>(type: "INTEGER", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    IsRoadBike = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ActualTime = table.Column<string>(type: "TEXT", nullable: false),
+                    TotalSeconds = table.Column<double>(type: "REAL", nullable: false),
+                    AvgSpeed = table.Column<double>(type: "REAL", nullable: true),
+                    Position = table.Column<int>(type: "INTEGER", nullable: true),
+                    PointsAwarded = table.Column<int>(type: "INTEGER", nullable: true),
+                    StandardTime = table.Column<string>(type: "TEXT", nullable: true),
+                    TimeDelta = table.Column<string>(type: "TEXT", nullable: true),
+                    HandicapGenerated = table.Column<string>(type: "TEXT", nullable: true),
+                    HandicapUsed = table.Column<string>(type: "TEXT", nullable: true),
+                    HCAdjustedTime = table.Column<string>(type: "TEXT", nullable: true),
+                    HCRank = table.Column<int>(type: "INTEGER", nullable: true),
+                    Eligibility = table.Column<string>(type: "TEXT", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rides", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rides");
+        }
+    }
+}
