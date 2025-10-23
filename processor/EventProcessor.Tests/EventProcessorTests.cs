2f78285885c7ee07abf641c35444f3d21d13997d
Mike Ives <mbives+wvcc@gmail.com>
2025-10-23 20:38:50 +0100

    Creates EventProcessorTests class

---
+++ processor/EventProcessor.Tests/EventProcessorTests.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using ClubProcessor.Context;
+using ClubProcessor.Models;
+using ClubProcessor.Models.Enums;
+using ClubProcessor.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Xunit;
+
+namespace EventProcessor.Tests
+{
+    internal class EventProcessorTests
+    {
+    }
+}
