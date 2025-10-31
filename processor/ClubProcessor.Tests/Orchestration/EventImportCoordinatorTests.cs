b51106a86e2bd350afcabf2c10d43adca19411fa
Mike Ives <mbives+wvcc@gmail.com>
2025-10-31 12:38:44 +0000

    Test pass

--- processor/ClubProcessor.Tests/Orchestration/EventImportCoordinatorTests.cs
+++ processor/ClubProcessor.Tests/Orchestration/EventImportCoordinatorTests.cs
@@ -41,9 +41,10 @@ public class EventImportCoordinatorTests
         {
             public string CompetitionName => "Stub";
 
-            public void ApplyScores(int eventNumber, List<Ride> rides, Func<int, int> pointsForPosition)
+            public int ApplyScores(int eventNumber, List<Ride> rides, Func<int, int> pointsForPosition)
             {
                 // No-op
+                return 0;
             }
         }
     }
