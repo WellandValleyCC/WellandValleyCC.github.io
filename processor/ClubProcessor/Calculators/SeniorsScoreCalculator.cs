3edc0d4159c1cd750d6e7df359af748aa9760888
Mike Ives <mbives+wvcc@gmail.com>
2025-11-01 16:43:16 +0000

    Scores calculators refactored (Juveniles and Seniors)

--- processor/ClubProcessor/Calculators/SeniorsScoreCalculator.cs
+++ processor/ClubProcessor/Calculators/SeniorsScoreCalculator.cs
@@ -1,75 +1,25 @@
-﻿using ClubProcessor.Interfaces;
-using ClubProcessor.Models;
+﻿using ClubProcessor.Models;
 using ClubProcessor.Models.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ClubProcessor.Calculators
 {
     /// <summary>
     /// Scores all FirstClaim and Honorary members based on raw ride time.
     /// </summary>
-    public class SeniorsScoreCalculator : ICompetitionScoreCalculator, IRideProcessor
+    internal class SeniorsScoreCalculator : BaseCompetitionScoreCalculator
     {
-        public string CompetitionName => "Seniors";
+        public override string CompetitionName => "Seniors";
 
-        private readonly Func<int, int> pointsForPosition;
-        public SeniorsScoreCalculator(Func<int, int> pointsForPosition)
+        public SeniorsScoreCalculator(Func<int, int> pointsForPosition) : base(pointsForPosition) { }
+
+        protected override bool IsEligible(Ride r) =>
+            r.Competitor is { ClaimStatus: ClaimStatus.FirstClaim or ClaimStatus.Honorary } &&
+            r.Eligibility == RideEligibility.Valid;
+
+        protected override void AssignPoints(Ride r, int position, double points)
         {
-            this.pointsForPosition = pointsForPosition;
-        }
-
-        public int ApplyScores(int eventNumber, List<Ride> rides, Func<int, int> pointsForPosition)
-        {
-            var eligibleRides = rides
-                .Where(r => r.Competitor != null && r.EventNumber == eventNumber &&
-                            r.Competitor.ClaimStatus is ClaimStatus.FirstClaim or ClaimStatus.Honorary &&
-                            r.Eligibility == RideEligibility.Valid)
-                .OrderBy(r => r.TotalSeconds)
-                .ToList();
-
-            var position = 1;
-            int i = 0;
-
-            while (i < eligibleRides.Count)
-            {
-                var currentTime = eligibleRides[i].TotalSeconds;
-
-                // Find all riders tied at this time
-                var tiedGroup = eligibleRides
-                    .Skip(i)
-                    .TakeWhile(r => r.TotalSeconds == currentTime)
-                    .ToList();
-
-                int tieCount = tiedGroup.Count;
-
-                // Calculate total points for the positions consumed by this tie
-                int totalPoints = Enumerable.Range(position, tieCount)
-                    .Select(pointsForPosition)
-                    .Sum();
-
-                //int sharedPoints = (totalPoints + tieCount - 1) / tieCount; // round fractions up
-                //int sharedPoints = (int)Math.Round((double)totalPoints / tieCount, MidpointRounding.AwayFromZero);
-                double sharedPoints = (double)totalPoints / tieCount;
-
-                foreach (var ride in tiedGroup)
-                {
-                    ride.SeniorsPosition = position;
-                    ride.SeniorsPoints = sharedPoints;
-                }
-
-                // Advance position and index by the size of the tie group
-                position += tieCount;
-                i += tieCount;
-            }
-
-            return eligibleRides.Count;
-        }
-
-        public int ProcessEvent(int eventNumber, List<Ride> eventRides)
-        {
-            return ApplyScores(eventNumber, eventRides, pointsForPosition);
+            r.SeniorsPosition = position;
+            r.SeniorsPoints = points;
         }
     }
 }
