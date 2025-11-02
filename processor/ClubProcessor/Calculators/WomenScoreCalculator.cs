0867cf92613f8533172536069fc3d7b4526bc840
Mike Ives <mbives+wvcc@gmail.com>
2025-11-02 10:47:56 +0000

    Three calculators implemented RoadBikeMen, RoadBikeWomen, Women
    Test fail - suspect test data

---
+++ processor/ClubProcessor/Calculators/WomenScoreCalculator.cs
@@ -0,0 +1,22 @@
+﻿using ClubProcessor.Models;
+using ClubProcessor.Models.Enums;
+
+namespace ClubProcessor.Calculators
+{
+    internal class WomenScoreCalculator : BaseCompetitionScoreCalculator
+    {
+        public override string CompetitionName => "Women";
+
+        public WomenScoreCalculator(Func<int, int> pointsForPosition) : base(pointsForPosition) { }
+
+        protected override bool IsEligible(Ride r) =>
+            r.Competitor is { ClaimStatus: ClaimStatus.FirstClaim or ClaimStatus.Honorary, IsFemale: true } &&
+            r.Eligibility == RideEligibility.Valid;
+
+        protected override void AssignPoints(Ride r, int position, double points)
+        {
+            r.WomenPosition = position;
+            r.WomenPoints = points;
+        }
+    }
+}
