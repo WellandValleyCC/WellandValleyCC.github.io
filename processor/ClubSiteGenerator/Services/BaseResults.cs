07f4903b4cc586bc6d71c95ed0ae5ce86757e321
Mike Ives <mbives+wvcc@gmail.com>
2025-11-12 12:08:27 +0000

    Initial scaffolding of results generation classes

---
+++ processor/ClubSiteGenerator/Services/BaseResults.cs
@@ -0,0 +1,47 @@
+﻿using ClubCore.Models;
+
+namespace ClubSiteGenerator.Services
+{
+    public abstract class BaseResults
+    {
+        protected readonly List<Ride> Rides;
+        protected readonly List<Competitor> Competitors;
+        protected readonly List<CalendarEvent> CalendarEvents;
+
+        protected BaseResults(List<Ride> rides, List<Competitor> competitors, List<CalendarEvent> calendarEvents)
+        {
+            Rides = rides;
+            Competitors = competitors;
+            CalendarEvents = calendarEvents;
+        }
+
+        // Friendly name for output file
+        public abstract string Name { get; }
+
+        // Query logic: which rides belong in this result set
+        public abstract IEnumerable<Ride> Query();
+
+        // Shape into a table model
+        public HtmlTable CreateTable()
+        {
+            var headers = new List<string> { "Pos", "Name", "Club", "Time", "Points" };
+            var rows = Query()
+                .OrderBy(r => r.Position)
+                .Select(r =>
+                {
+                    var competitor = Competitors.FirstOrDefault(c => c.ClubNumber == r.ClubNumber);
+                    return new List<string>
+                    {
+                        r.Position.ToString(),
+                        competitor?.Name ?? "Unknown",
+                        competitor?.Club ?? "",
+                        TimeSpan.FromSeconds(r.TotalSeconds).ToString(@"mm\:ss"),
+                        r.Points.ToString()
+                    };
+                })
+                .ToList();
+
+            return new HtmlTable(headers, rows);
+        }
+    }
+}
