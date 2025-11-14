36ff6764f90a1bcef1bf12e03085fd33edc0ebb0
Mike Ives <mbives+wvcc@gmail.com>
2025-11-14 10:08:32 +0000

    Adds EventEligibleRidersRank and EventEligibleRoadBikeRidersRank
    Additional properties on RIde object.
    New Calculator classes- some renaming of existing calculators
    Migration added

--- processor/ClubProcessor/Calculators/EventRankAllRiderCalculator.cs
+++ processor/ClubProcessor/Calculators/EventRankAllRiderCalculator.cs
@@ -3,7 +3,7 @@ using ClubCore.Models.Enums;
 
 namespace ClubProcessor.Calculators
 {
-    public class AllRiderEventRankCalculator : BaseEventRankCalculator
+    public class EventRankAllRidersCalculator : BaseEventRankCalculator
     {
         protected override bool IsEligible(Ride ride) => ride.Eligibility == RideEligibility.Valid;
         protected override void AssignRank(Ride ride, int rank) => ride.EventRank = rank;
---
+++ processor/ClubProcessor/Calculators/EventRankRoadBikesCalculator.cs
@@ -0,0 +1,13 @@
+﻿using ClubCore.Models;
+using ClubCore.Models.Enums;
+
+namespace ClubProcessor.Calculators
+{
+    public class EventRankRoadBikesCalculator : BaseEventRankCalculator
+    {
+        protected override bool IsEligible(Ride ride) => 
+            ride.Eligibility == RideEligibility.Valid && 
+            ride.IsRoadBike;
+        protected override void AssignRank(Ride ride, int rank) => ride.EventRoadBikeRank = rank;
+    }
+}
