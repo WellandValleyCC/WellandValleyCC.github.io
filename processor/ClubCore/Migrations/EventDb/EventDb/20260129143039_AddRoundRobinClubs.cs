92c89fb77d0bc3bb02e516b208dfa8251a497b09
Mike Ives <mbives+wvcc@gmail.com>
2026-01-29 14:32:50 +0000

    Adds RoundRobinClub class with initial migration

---
+++ processor/ClubCore/Migrations/EventDb/EventDb/20260129143039_AddRoundRobinClubs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubProcessor.Migrations.EventDb
+{
+    /// <inheritdoc />
+    public partial class AddRoundRobinClubs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ processor/ClubCore/Models/RoundRobinClub.cs
@@ -0,0 +1,10 @@
+﻿namespace ClubCore.Models
+{
+    public class RoundRobinClub
+    {
+        public int Id { get; set; }
+        public string ShortName { get; set; } = default!;
+        public string FullName { get; set; } = default!;
+        public string WebsiteUrl { get; set; } = default!;
+    }
+}
