7d0b554288e91dcc05c90d7b4a133d40dbdbf5f2
Mike Ives <mbives+wvcc@gmail.com>
2026-01-30 20:08:45 +0000

    Adds RoundRobinWomenScoreCalculator

---
+++ processor/ClubProcessor/Calculators/RoundRobinWomenScoreCalculator.cs
@@ -0,0 +1,31 @@
+﻿using ClubCore.Models;
+using ClubCore.Models.Enums;
+
+namespace ClubProcessor.Calculators
+{
+    /// <summary>
+    /// Scores all female riders from WVCC or any of the RoundRobin clubs based on raw ride time.
+    /// </summary>
+    internal class RoundRobinWomenScoreCalculator : BaseCompetitionScoreCalculator
+    {
+        public override string CompetitionName => "RoundRobinWomen";
+
+        public RoundRobinWomenScoreCalculator(Func<int, int> pointsForPosition) : base(pointsForPosition) { }
+
+        protected override bool IsEligible(Ride r) =>
+            HasParticipant(r) &&
+            (r.Competitor is { IsFemale: true } c || r.RoundRobinRider is { IsFemale: true } rr) &&
+            r.Status == RideStatus.Valid;
+        
+        private static bool HasParticipant(Ride r) =>
+            r.Competitor is not null || r.RoundRobinClub is not null;
+
+        protected override void AssignPoints(Ride r, int position, double points)
+        {
+            r.RoundRobinWomenPosition = position;
+            r.RoundRobinWomenPoints = points;
+        }
+    }
+}
+
+
