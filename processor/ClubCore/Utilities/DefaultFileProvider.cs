88fc4e99100a3c4f34f0ba64e99d0e3a748eec74
Mike Ives <mbives+wvcc@gmail.com>
2026-02-13 11:40:34 +0000

    Adds IFileProvider and DefaultFileProvider - to support unit tests

---
+++ processor/ClubCore/Utilities/DefaultFileProvider.cs
@@ -0,0 +1,15 @@
+﻿using ClubCore.Interfaces;
+
+namespace ClubCore.Utilities
+{
+    public class DefaultFileProvider : IFileProvider
+    {
+        public bool Exists(string path) => File.Exists(path);
+
+        public void Copy(string source, string destination, bool overwrite)
+            => File.Copy(source, destination, overwrite);
+
+        public string[] GetFiles(string directory)
+            => Directory.GetFiles(directory);
+    }
+}
