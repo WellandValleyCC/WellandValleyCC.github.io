c68cbeb58601068234fcebdf272c0083f32fc9e4
Mike Ives <mbives+wvcc@gmail.com>
2026-02-13 12:03:24 +0000

    Default helper classes unit tests

---
+++ processor/ClubSiteGenerator.Tests/Utilities/DefaultDirectoryCopyHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using ClubCore.Interfaces;
+using ClubCore.Utilities;
+using ClubSiteGenerator.Interfaces;
+using NSubstitute;
+
+namespace ClubSiteGenerator.Tests.Utilities
+{
+    public class DefaultDirectoryCopyHelperTests
+    {
+        [Fact]
+        public void Delegates_To_Inner_Helper()
+        {
+            var directoryProvider = Substitute.For<IDirectoryProvider>();
+            var fileProvider = Substitute.For<IFileProvider>();
+            var log = Substitute.For<ILog>();
+
+            var helper = new DefaultDirectoryCopyHelper(
+                directoryProvider,
+                fileProvider,
+                log
+            );
+
+            helper.CopyRecursive("src", "dest", new[] { ".md" });
+
+            directoryProvider.Received().Exists("src");
+        }
+    }
+}
---
+++ processor/ClubSiteGenerator.Tests/Utilities/DefaultFileProviderTests.cs
@@ -0,0 +1,47 @@
+﻿using ClubCore.Utilities;
+using FluentAssertions;
+
+namespace ClubSiteGenerator.Tests.Utilities
+{
+    public class DefaultFileProviderTests
+    {
+        [Fact]
+        public void Copy_Creates_Destination_File()
+        {
+            var provider = new DefaultFileProvider();
+
+            var temp = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(temp);
+
+            var src = Path.Combine(temp, "a.txt");
+            var dest = Path.Combine(temp, "b.txt");
+
+            File.WriteAllText(src, "hello");
+
+            provider.Copy(src, dest, overwrite: true);
+
+            File.Exists(dest).Should().BeTrue();
+            File.ReadAllText(dest).Should().Be("hello");
+
+            Directory.Delete(temp, recursive: true);
+        }
+
+        [Fact]
+        public void GetFiles_Returns_Files()
+        {
+            var provider = new DefaultFileProvider();
+
+            var temp = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(temp);
+
+            var file = Path.Combine(temp, "x.txt");
+            File.WriteAllText(file, "data");
+
+            var result = provider.GetFiles(temp);
+
+            result.Should().Contain(file);
+
+            Directory.Delete(temp, recursive: true);
+        }
+    }
+}
