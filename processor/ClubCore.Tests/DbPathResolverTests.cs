e82745c69aeff6e17e5afc3d8bcb3c9a3bcc090d
Mike Ives <mbives+wvcc@gmail.com>
2026-02-13 16:29:21 +0000

    Adds DbPathResolverTests

---
+++ processor/ClubCore.Tests/DbPathResolverTests.cs
@@ -0,0 +1,43 @@
+﻿using ClubCore.Interfaces;
+using ClubCore.Utilities;
+using ClubSiteGenerator.Interfaces;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace ClubCore.Tests.Utilities
+{
+    public class DbPathResolverTests
+    {
+        private readonly IFolderLocator folderLocator = Substitute.For<IFolderLocator>();
+
+        private IDbPathResolver CreateResolver() =>
+            new DbPathResolver(folderLocator);
+
+        [Fact]
+        public void Builds_Competitor_Db_Path()
+        {
+            folderLocator.FindGitRepoRoot().Returns("root");
+
+            var resolver = CreateResolver();
+            var result = resolver.GetCompetitorDbPath("2024");
+
+            result.Should().Be(
+                Path.Combine("root", PathTokens.DataFolder, "club_competitors_2024.db")
+            );
+        }
+
+        [Fact]
+        public void Builds_Event_Db_Path()
+        {
+            folderLocator.FindGitRepoRoot().Returns("root");
+
+            var resolver = CreateResolver();
+            var result = resolver.GetEventDbPath("2024");
+
+            result.Should().Be(
+                Path.Combine("root", PathTokens.DataFolder, "club_events_2024.db")
+            );
+        }
+    }
+}
