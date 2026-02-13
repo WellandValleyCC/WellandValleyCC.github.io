e13f478965661fe2c0afd2ef5971806b11414ef8
Mike Ives <mbives+wvcc@gmail.com>
2026-02-13 11:43:54 +0000

    extends DefaultDirectoryProvider

--- processor/ClubSiteGenerator/Utilities/DefaultDirectoryProvider.cs
+++ processor/ClubSiteGenerator/Utilities/DefaultDirectoryProvider.cs
@@ -4,7 +4,12 @@ namespace ClubCore.Utilities
 {
     public class DefaultDirectoryProvider : IDirectoryProvider
     {
+        public bool Exists(string path) => Directory.Exists(path);
+
+        public void CreateDirectory(string path) => Directory.CreateDirectory(path);
+
         public string[] GetDirectories(string path)
             => Directory.GetDirectories(path);
+
     }
 }
