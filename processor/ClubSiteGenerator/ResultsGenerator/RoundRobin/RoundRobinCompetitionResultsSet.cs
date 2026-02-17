ade69345ef25a06edf9eade0a23871954a31d3a3
Mike Ives <mbives+wvcc@gmail.com>
2026-02-17 14:50:24 +0000

    Adds RR-specific competition base class

---
+++ processor/ClubSiteGenerator/ResultsGenerator/RoundRobin/RoundRobinCompetitionResultsSet.cs
@@ -0,0 +1,23 @@
+﻿using ClubCore.Models;
+using ClubSiteGenerator.Models;
+using ClubSiteGenerator.Models.Enums;
+
+namespace ClubSiteGenerator.ResultsGenerator.RoundRobin
+{
+    public abstract class RoundRobinCompetitionResultsSet : ResultsSet
+    {
+        public IReadOnlyList<CompetitorResult> ScoredRides { get; }
+
+        protected RoundRobinCompetitionResultsSet(
+            IEnumerable<CalendarEvent> calendar,
+            IEnumerable<CompetitorResult> scoredRides)
+            : base(calendar)
+        {
+            ScoredRides = scoredRides.ToList().AsReadOnly();
+        }
+
+        public abstract RoundRobinCompetitionType CompetitionType { get; }
+
+        public abstract string EligibilityStatement { get; }
+    }
+}
