dfdeeac73565297db0a76c6b4a7efc104ff11798
Mike Ives <mbives+wvcc@gmail.com>
2026-06-22 16:18:24 +0100

    Unit tests extended for isPostponed

--- processor/ClubProcessor.Tests/CalendarImporterTests.cs
+++ processor/ClubProcessor.Tests/CalendarImporterTests.cs
@@ -30,14 +30,14 @@ namespace ClubProcessor.Tests
             Directory.CreateDirectory("test-data");
             File.WriteAllText(
                 testCsvPath,
-                "Event Number,Date,Start time,Event Name,Round Robin Club,Miles,Location / Course,Hill Climb,Club Championship,Non-Standard 10,Evening 10,Hard Ride Series,Round Robin Event,Sheet Name,isCancelled\n" +
-                "1,2025-04-01,18:30,Medbourne 9.5mile Hardride TT,,9.5,Medbourne,,Y,Y,,Y,,Event_01,\n" +
-                "2,2025-04-06,09:00,Medbourne 9.5mile NDCA Hardride TT,,9.5,Medbourne,,Y,Y,,Y,,Event_02,\n" +
-                "3,2025-04-15,18:30,Hallaton 5 5mile TT,,5.0,Hallaton 5,,Y,Y,,,,Event_03,\n" +
-                "4,2025-04-20,08:00,Kibworth 25mile TT,,25.0,Kibworth,,Y,Y,,,,Event_04,\n" +
-                "5,2025-04-29,18:30,Tour of Langtons 10mile TT,HCRC,10.0,Tour of Langtons,,Y,,Y,,Y,Event_05,\n" +
-                "6,2025-05-06,18:30,Tur Langton/Hallaton 5+5mile TT,,10.0,Tur Langton/Hallaton,,Y,Y,,,,Event_06,\n" +
-                "23,2025-09-07,10:30:00,Interclub Hillclimb RFW,RFW,0.88,Nevill Holt ,Y,Y,Y,,,,Event_23,\n");
+                "Event Number,Date,Start time,Event Name,Round Robin Club,Miles,Location / Course,Hill Climb,Club Championship,Non-Standard 10,Evening 10,Hard Ride Series,Round Robin Event,Sheet Name,isCancelled,isPostponed\n" +
+                "1,2025-04-01,18:30,Medbourne 9.5mile Hardride TT,,9.5,Medbourne,,Y,Y,,Y,,Event_01,,\n" +
+                "2,2025-04-06,09:00,Medbourne 9.5mile NDCA Hardride TT,,9.5,Medbourne,,Y,Y,,Y,,Event_02,,\n" +
+                "3,2025-04-15,18:30,Hallaton 5 5mile TT,,5.0,Hallaton 5,,Y,Y,,,,Event_03,,\n" +
+                "4,2025-04-20,08:00,Kibworth 25mile TT,,25.0,Kibworth,,Y,Y,,,,Event_04,,\n" +
+                "5,2025-04-29,18:30,Tour of Langtons 10mile TT,HCRC,10.0,Tour of Langtons,,Y,,Y,,Y,Event_05,,\n" +
+                "6,2025-05-06,18:30,Tur Langton/Hallaton 5+5mile TT,,10.0,Tur Langton/Hallaton,,Y,Y,,,,Event_06,Y,\n" +
+                "23,2025-09-07,10:30:00,Interclub Hillclimb RFW,RFW,0.88,Nevill Holt ,Y,Y,Y,,,,Event_23,,Y\n");
 
 
             // Act
@@ -54,37 +54,37 @@ namespace ClubProcessor.Tests
                 new {
                     Number = 1, Date = new DateTime(2025, 4, 1), Time = TimeSpan.FromHours(18.5),
                     Name = "Medbourne 9.5mile Hardride TT", RoundRobinClub = "", Miles = 9.5, Location = "Medbourne",
-                    HillClimb = false, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = true, RoundRobinEvent = false, Cancelled = false
+                    HillClimb = false, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = true, RoundRobinEvent = false, Cancelled = false, Postponed = false
                 },
                 new {
                     Number = 2, Date = new DateTime(2025, 4, 6), Time = TimeSpan.FromHours(9),
                     Name = "Medbourne 9.5mile NDCA Hardride TT", RoundRobinClub = "", Miles = 9.5, Location = "Medbourne",
-                    HillClimb = false, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = true, RoundRobinEvent = false, Cancelled = false
+                    HillClimb = false, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = true, RoundRobinEvent = false, Cancelled = false, Postponed = false
                 },
                 new {
                     Number = 3, Date = new DateTime(2025, 4, 15), Time = TimeSpan.FromHours(18.5),
                     Name = "Hallaton 5 5mile TT", RoundRobinClub = "", Miles = 5.0, Location = "Hallaton 5",
-                    HillClimb = false, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = false, RoundRobinEvent = false, Cancelled = false
+                    HillClimb = false, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = false, RoundRobinEvent = false, Cancelled = false, Postponed = false
                 },
                 new {
                     Number = 4, Date = new DateTime(2025, 4, 20), Time = TimeSpan.FromHours(8),
                     Name = "Kibworth 25mile TT", RoundRobinClub = "", Miles = 25.0, Location = "Kibworth",
-                    HillClimb = false,  ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = false, RoundRobinEvent = false, Cancelled = false
+                    HillClimb = false,  ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = false, RoundRobinEvent = false, Cancelled = false, Postponed = false
                 },
                 new {
                     Number = 5, Date = new DateTime(2025, 4, 29), Time = TimeSpan.FromHours(18.5),
                     Name = "Tour of Langtons 10mile TT", RoundRobinClub = "HCRC", Miles = 10.0, Location = "Tour of Langtons",
-                    HillClimb = false, ClubChamp = true, NonStd10 = false, Evening10 = true, HardRide = false, RoundRobinEvent = true, Cancelled = false
+                    HillClimb = false, ClubChamp = true, NonStd10 = false, Evening10 = true, HardRide = false, RoundRobinEvent = true, Cancelled = false, Postponed = false
                 },
                 new {
                     Number = 6, Date = new DateTime(2025, 5, 6), Time = TimeSpan.FromHours(18.5),
                     Name = "Tur Langton/Hallaton 5+5mile TT", RoundRobinClub = "", Miles = 10.0, Location = "Tur Langton/Hallaton",
-                    HillClimb = false, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = false, RoundRobinEvent = false, Cancelled = false
+                    HillClimb = false, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = false, RoundRobinEvent = false, Cancelled = true, Postponed = false
                 },
                 new {
                     Number = 23, Date = new DateTime(2025, 9, 7), Time = TimeSpan.FromHours(10.5),
                     Name = "Interclub Hillclimb RFW", RoundRobinClub = "RFW", Miles = 0.88, Location = "Nevill Holt",
-                    HillClimb = true, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = false, RoundRobinEvent = false, Cancelled = false
+                    HillClimb = true, ClubChamp = true, NonStd10 = true, Evening10 = false, HardRide = false, RoundRobinEvent = false, Cancelled = false, Postponed = true
                 }
             };
 
@@ -137,8 +137,8 @@ namespace ClubProcessor.Tests
             // Insert a CSV row with an INVALID Round Robin Club: "XYZ"
             File.WriteAllText(
                 testCsvPath,
-                "Event Number,Date,Start time,Event Name,Round Robin Club,Miles,Location / Course,Hill Climb,Club Championship,Non-Standard 10,Evening 10,Hard Ride Series,Round Robin Event,Sheet Name,isCancelled\n" +
-                "1,2025-04-01,18:30,Invalid Club Test,XYZ,10.0,Medbourne,,Y,,Y,,Y,Event_01,\n"
+                "Event Number,Date,Start time,Event Name,Round Robin Club,Miles,Location / Course,Hill Climb,Club Championship,Non-Standard 10,Evening 10,Hard Ride Series,Round Robin Event,Sheet Name,isCancelled,isPostponed\n" +
+                "1,2025-04-01,18:30,Invalid Club Test,XYZ,10.0,Medbourne,,Y,,Y,,Y,Event_01,,\n"
             );
 
             // Act
@@ -175,8 +175,8 @@ namespace ClubProcessor.Tests
             // Insert a CSV row with an INVALID Round Robin Club: "XYZ"
             File.WriteAllText(
                 testCsvPath,
-                "Event Number,Date,Start time,Event Name,Round Robin Club,Miles,Location / Course,Hill Climb,Club Championship,Non-Standard 10,Evening 10,Hard Ride Series,Round Robin Event,Sheet Name,isCancelled\n" +
-                "1,2025-04-01,18:30,Invalid Club Test,WVCC,10.0,Medbourne,,Y,,Y,,Y,Event_01,\n"
+                "Event Number,Date,Start time,Event Name,Round Robin Club,Miles,Location / Course,Hill Climb,Club Championship,Non-Standard 10,Evening 10,Hard Ride Series,Round Robin Event,Sheet Name,isCancelled,isPostponed\n" +
+                "1,2025-04-01,18:30,Invalid Club Test,WVCC,10.0,Medbourne,,Y,,Y,,Y,Event_01,,\n"
             );
 
             // Act
@@ -211,8 +211,8 @@ namespace ClubProcessor.Tests
             // Insert a CSV row with an INVALID Round Robin Club: "XYZ"
             File.WriteAllText(
                 testCsvPath,
-                "Event Number,Date,Start time,Event Name,Round Robin Club,Miles,Location / Course,Hill Climb,Club Championship,Non-Standard 10,Evening 10,Hard Ride Series,Round Robin Event,Sheet Name,isCancelled\n" +
-                "1,2025-04-01,18:30,Invalid Club Test,\"HCRC,WVCC\",10.0,Medbourne,,Y,,Y,,Y,Event_01,\n"
+                "Event Number,Date,Start time,Event Name,Round Robin Club,Miles,Location / Course,Hill Climb,Club Championship,Non-Standard 10,Evening 10,Hard Ride Series,Round Robin Event,Sheet Name,isCancelled,isPostponed\n" +
+                "1,2025-04-01,18:30,Invalid Club Test,\"HCRC,WVCC\",10.0,Medbourne,,Y,,Y,,Y,Event_01,,\n"
             );
 
             // Act
