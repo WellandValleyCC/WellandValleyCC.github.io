4f53c7f00b180776fae132fd0914cf82a6829920
Mike Ives <mbives+wvcc@gmail.com>
2026-07-18 16:13:30 +0100

    Add migration for new IsShortenedTen column on CalendarEvents table
    PM> Add-Migration AddShortenedTenFlag -Project ClubCore -StartupProject ClubProcessor -Context EventDbContext
    Build started...
    Build succeeded.
    [INFO] Git repo root found at: C:\repos\wvcc\WellandValleyCC.github.io
    [INFO] Creating EventDbContext for year 2025 → C:\repos\wvcc\WellandValleyCC.github.io\data\club_events_2025.db

---
+++ processor/ClubCore/Migrations/EventDb/EventDb/20260718151103_AddShortenedTenFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubProcessor.Migrations.EventDb
+{
+    /// <inheritdoc />
+    public partial class AddShortenedTenFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsShortenedTen",
+                table: "CalendarEvents",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsShortenedTen",
+                table: "CalendarEvents");
+        }
+    }
+}
